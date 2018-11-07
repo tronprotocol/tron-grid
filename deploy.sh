66ab328d850e656343d8ec17e21c02fb8315ad9f
jialiang <ericcareer@hotmail.com>
2018-11-07 15:51:21 -0800

    add sleep time

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ else
       pid=`ps -ef |grep trongrid |grep -v grep |awk '{print $2}'`
       if [ -n "$pid" ]; then
         kill -15 $pid
-        echo "The java-tron process is exiting, it may take some time, forcing the exit may cause damage to the database, please wait patiently..."
+        echo "ending trongrid process"
         sleep 1
       else
         echo "trongrid killed successfully!"
@@ -16,6 +16,7 @@ else
       fi
     done
     nohup java -jar target/trongrid-0.0.1-SNAPSHOT.jar 2> grid.log &
+    sleep 10
     echo "ok!"
 fi
 
